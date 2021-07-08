f265c0609d8c7ec73bb00fd2083dceb7ac470e6a
Maher <cosa-nostra.meher@live.fr>
2021-07-08 15:52:06 +0200

    with pre load exercise

---
+++ app/src/main/java/com/dna/plank/Activities/BeforeMainActivity.java
@@ -0,0 +1,28 @@
+package com.dna.plank.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.dna.plank.R;
+
+public class BeforeMainActivity extends AppCompatActivity {
+    private  final int DISPLAY =1000 ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.before_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(BeforeMainActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },DISPLAY);
+    }
+}
